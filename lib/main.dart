9ef68d5f011cbd77231f1a5eb2c798e74f801e3c
Phoo Pwint Aung <p-aung@isgm2.com>
2019-11-13 15:24:23 +0630

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
+    show CalendarCarousel;
+import 'package:flutter_calendar_carousel/classes/event_list.dart';
+import 'package:intl/intl.dart' show DateFormat;
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget
+{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-
-  setState(DateTime date)
+  Widget build(BuildContext context)
   {
-
-  }
-  Widget widget()
-  {
-    return Container
+    return new MaterialApp
     (
-      margin: EdgeInsets.symmetric(horizontal: 16.0),
-      child: CalendarCarousel<Event>
+      title: 'My Diary',
+      theme: new ThemeData
       (
-        onDayPressed: (DateTime date, List<Event> events)
-        {
-          this.setState(() => _currentDate = date);
-        },
-        weekendTextStyle: TextStyle
-        (
-          color: Colors.red,
-        ),
-        thisMonthDayBorderColor: Colors.grey,
-
-        customDayBuilder:
-        (
-          bool isSelectable,
-          int index,
-            bool isSelectedDay,
-            bool isToday,
-            bool isPrevMonthDay,
-            TextStyle textStyle,
-            bool isNextMonthDay,
-            bool isThisMonthDay,
-            DateTime day,
-        )
-        {
-          if(day.day ==15)
-            {
-              return Center
-                (
-                child: Icon(Icons.local_airport),
-              );
-            }
-          else
-          {
-            return null;
-          }
-        },
-        weekFormat: false,
-        markedDatesMap: _markedDateMap,
-        height: 420.0,
-        selectedDateTime: _currentDate,
-        daysHaveCircularBorder: false,
-      )
+        primarySwatch: Colors.purple,
+      ),
+      home: new MyHomePage(title: 'MyDiary'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget
+{
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -96,41 +40,308 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+{
+  DateTime _currentDate = DateTime(2019, 2, 3);
+  DateTime _currentDate2 = DateTime(2019, 2, 3);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String _currentMonth = '';
+
+  static Widget _eventIcon = new Container
+  (
+    decoration: new BoxDecoration
+    (
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(1000)),
+      border: Border.all(color: Colors.blue, width: 2.0)
+  ),
+    child: new Icon
+    (
+      Icons.person,
+      color: Colors.amber,
+    )
+  );
+
+  EventList<Event> _markedDateMap = new EventList<Event>
+  (
+    events:
+    {
+      new DateTime(2019, 2, 10):
+          [
+            new Event
+            (
+              date: new DateTime(2019, 2, 10),
+              title: 'Event 1',
+              icon: _eventIcon,
+              dot: Container
+              (
+                margin: EdgeInsets.symmetric(horizontal: 1.0),
+                color: Colors.red,
+                height: 5.0,
+                width: 5.0,
+              ),
+            ),
+            new Event
+            (
+              date: new DateTime(2019, 2, 10),
+              title: 'Event 2',
+              icon: _eventIcon,
+            ),
+            new Event
+            (
+              date: new DateTime(2019, 2, 10),
+              title: 'Event 3',
+              icon: _eventIcon,
+
+            )
+          ]
+    }
+  );
+
+  CalendarCarousel _calendarCarousel, _calendarCarouselNoHeader;
+
+  @override
+  void initState()
+  {
+    _markedDateMap.add
+    (
+      new DateTime(2019, 2, 25),
+      new Event
+      (
+        date: new DateTime(2019, 2, 25),
+        title: 'Event 5',
+        icon: _eventIcon,
+      )
+    );
+
+    _markedDateMap.add
+    (
+      new DateTime(2019, 2, 10),
+      new Event
+      (
+        date: new DateTime(2019, 2, 10),
+        title: 'Event 4',
+        icon: _eventIcon,
+      )
+    );
+    
+    _markedDateMap.addAll(new DateTime(2019, 2, 11),
+    [
+      new Event
+      (
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 1',
+        icon: _eventIcon,
+      ),
+
+      new Event
+      (
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 2',
+        icon: _eventIcon,
+      ),
+
+      new Event
+      (
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 3',
+        icon: _eventIcon,
+      ),
+
+      new Event
+      (
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 4',
+        icon: _eventIcon,
+      ),
+
+      new Event
+      (
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 23',
+        icon: _eventIcon,
+      ),
+
+      new Event
+      (
+        date: new DateTime(2019, 2, 11),
+        title: 'Event 123',
+        icon: _eventIcon,
+      )
+    ]);
+    super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
+
+    _calendarCarousel = CalendarCarousel<Event>
+    (
+      onDayPressed: (DateTime date, List<Event> events)
+      {
+        this.setState(()=> _currentDate = date);
+        events.forEach((event) => print(event.title));
+      },
+      weekendTextStyle: TextStyle
+      (
+        color: Colors.red,
+      ),
+
+      thisMonthDayBorderColor: Colors.grey,
+
+      headerText: 'Custom Header',
+
+      weekFormat: true,
+
+      markedDatesMap: _markedDateMap,
+
+      height: 200.0,
+
+      selectedDateTime: _currentDate2,
+
+      showIconBehindDayText: true,
+
+      customGridViewPhysics: NeverScrollableScrollPhysics(),
+
+      markedDateShowIcon: true,
+
+      markedDateIconMaxShown: 2,
+
+      selectedDayTextStyle: TextStyle
+      (
+        color: Colors.yellow,
+      ),
+
+      todayTextStyle: TextStyle
+      (
+        color: Colors.blue,
+      ),
+
+      markedDateIconBuilder: (event)
+      {
+        return event.icon;
+      },
+
+      minSelectedDate: _currentDate.subtract(Duration(days: 360)),
+
+      maxSelectedDate: _currentDate.add(Duration(days: 360)),
+
+      todayButtonColor: Colors.transparent,
+
+      todayBorderColor: Colors.green,
+
+      markedDateMoreShowTotal: false,
+
+    );
+
+    _calendarCarouselNoHeader = CalendarCarousel<Event>
+    (
+      todayBorderColor: Colors.green,
+      onDayPressed: (DateTime date, List<Event> events)
+      {
+        this.setState(() => _currentDate2 = date);
+        events.forEach((event) => print(event.title));
+      },
+      daysHaveCircularBorder: true,
+      showOnlyCurrentMonthDate: false,
+      weekendTextStyle: TextStyle
+      (
+        color: Colors.red,
+      ),
+
+      thisMonthDayBorderColor: Colors.grey,
+      weekFormat: false,
+
+      markedDatesMap: _markedDateMap,
+
+      height: 420.0,
+
+      selectedDateTime: _currentDate2,
+
+      customGridViewPhysics: NeverScrollableScrollPhysics(),
+
+      markedDateCustomShapeBorder: CircleBorder
+      (
+        side: BorderSide
+        (
+          color: Colors.yellow,
+        )
+      ),
+
+      markedDateCustomTextStyle: TextStyle
+      (
+        fontSize: 18,
+        color: Colors.blue,
+      ),
+
+      showHeader: false,
+
+      todayTextStyle: TextStyle
+      (
+        color: Colors.blue,
+      ),
+
+      todayButtonColor: Colors.yellow,
+
+      selectedDayTextStyle: TextStyle
+      (
+        color: Colors.yellow,
+      ),
+
+      minSelectedDate: _currentDate.subtract(Duration(days: 360)),
+      maxSelectedDate: _currentDate.add(Duration(days: 360)),
+
+      prevDaysTextStyle: TextStyle
+      (
+        fontSize: 16,
+        color: Colors.pinkAccent,
+      ),
+
+      inactiveDaysTextStyle: TextStyle
+      (
+        color: Colors.tealAccent,
+        fontSize: 16,
+      ),
+
+      onCalendarChanged: (DateTime date)
+      {
+        this.setState(() => _currentMonth = DateFormat.yMMM().format(date));
+      },
+
+      onDayLongPressed: (DateTime date)
+      {
+        print('Long pressed date $date');
+      },
+      );
+
+
+
+
+
+    return new Scaffold
+      (
+      appBar: new AppBar
+      (
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: new Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView
+      (
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Column
+          (
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -145,20 +356,83 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>
+          [
+            //Custom Icon
+            Container
+            (
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              child: _calendarCarousel,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            //Custom Icon without Header
+            Container
+            (
+              margin: EdgeInsets.only
+              (
+                top: 30.0,
+                bottom: 16.0,
+                left: 16.0,
+                right: 16.0,
+              ),
+
+              child: new Row
+              (
+                children: <Widget>
+                [
+                  Expanded
+                  (
+                    child: Text
+                    (
+                      _currentMonth,
+                      style: TextStyle
+                      (
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                  ),
+                  FlatButton
+                  (
+                    child: Text('PREV'),
+                    onPressed: ()
+                    {
+                      setState(()
+                      {
+                        _currentDate2 = _currentDate2.subtract(Duration(days: 30));
+                        _currentMonth = DateFormat.yMMM().format(_currentDate2);
+                      });
+                    },
+                  ),
+
+                  FlatButton
+                  (
+                    child: Text('NEXT'),
+                    onPressed: ()
+                    {
+                      setState(()
+                      {
+                        _currentDate2 = _currentDate2.add(Duration(days: 30));
+                        _currentMonth = DateFormat.yMMM().format(_currentDate2);
+                      });
+                    },
+                  )
+                ],
+              ),
             ),
-          ],
+
+            Container
+            (
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              child: _calendarCarouselNoHeader,
+            )
+
+          ]
+
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
